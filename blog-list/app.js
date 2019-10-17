fc86f17125037bd74f8453a41f21951d4222e5fe
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2019-10-18 01:44:01 +0300

    done Ex. 4.1-4.2 Blog list, steps1-2

---
+++ blog-list/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const config = require('./utils/config')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((err) => {
+    console.log('Error connection to MongoDB:', err.message)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
